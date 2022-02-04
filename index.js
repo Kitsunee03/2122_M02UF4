1a2d8cfc94b0ab19a7317cc6edf441d5eefd6b6f
enti <enti@enti.enti>
2022-02-04 11:26:29 +0100

    Cambios del index.js

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 #!/usr/bin/node
 
 let http = require("http");
+let mongo_client = require("mongodb").MongoClient;
 
-http.createServer(function(req,res) {
+let url = "mongodb://localhost/";
+console.log("Iniciando Script mongo_http");
+mongo_client.connect(url, function(error,conn){
+	console.log("Dentro de Mongo >:\)");
+	if (error){
+		console.log("Esto no furula TwT");
+		return;
+	}
+	let db=conn.db("tffhd");
+	let characters=db.collection("characters").find();
+	console.log(characters);
+});
+
+/*http.createServer(function(req,res) {
 	res.writeHead(200);
 
 	let saludo="<h1>esta wea rula de lujo >:\)</h1>";
 	res.end(saludo);
 
-}).listen(1095);
+}).listen(1095);*/
